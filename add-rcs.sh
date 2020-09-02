1a50ac6d3347f719d4fcda845eb59f7b6f6b5520
stein212 <stein212@users.noreply.github.com>
2020-09-02 16:54:18 +0800

    Update add-rcs.sh

--- add-rcs.sh
+++ add-rcs.sh
@@ -1,2 +1,9 @@
-curl -s -O https://raw.githubusercontent.com/stein212/linux-rcs/master/.inputrc
+echo "Downloading .profile"
 curl -s -O https://raw.githubusercontent.com/stein212/linux-rcs/master/.profile
+echo "Downloaded .profile"
+
+echo "Downloading .inputrc"
+curl -s -O https://raw.githubusercontent.com/stein212/linux-rcs/master/.inputrc
+echo "Downloaded .inputrc"
+
+echo "Done"
